b30ac82e0123f9a7d232bbc83d05f86e87769046
Shamail <zahis538@newschool.edu>
2018-10-23 10:11:09 -0400

    ok

--- js/01_intro.js
+++ js/01_intro.js
@@ -2,3 +2,14 @@ var addOne = function(number) {
   return number + 1;
 };
 
+/*
+
+generate an HSL color with a hue as an imput 
+in this format:
+"hsl(20,100%,100%)"
+*/
+
+var hslColor = function(hue) {
+	var color = "hsl(" + hue + ", 100%, 100%)"
+	return color;
+};
