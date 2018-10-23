825cbb81acaf452a59c954b6b62dc06687886e20
Shamail <zahis538@newschool.edu>
2018-10-23 11:30:04 -0400

    ok

--- js/03_random.js
+++ js/03_random.js
@@ -68,5 +68,5 @@ shape.style.height= height + "px";
 var h = randomNumber(0, 179);
 
 shape.style.background = hslColor (h, 90, 50);
-shape.style.borderColor = hslColor (h, 10, 90);
+shape.style.borderColor = hslColor (h, 100, 90);
 
